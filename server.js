8fabff0b6d7d2725e812df456dcdfc8acee4c3ca
Duraimurugan-H <hduraimurugan@gmail.com>
2025-07-07 22:06:08 +0530

    updated cors origin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "https://military-assets-management-eta.vercel.app"
+  "https://cinema-hall-admin.vercel.app"
 ];
 
 // CORS middleware
